7956e82b292b22c60bb6fec74ae88604ec3a1e47
Ashutosh Shukla <ashutosh.shukla@innovect.com>
2020-02-04 13:30:41 +0530

    1.Richardson Maturity Model level2 implementation. Added link of relevant urli in headers for controller.

--- src/main/java/org/innovect/assignment/controller/OrderProcessingController.java
+++ src/main/java/org/innovect/assignment/controller/OrderProcessingController.java
@@ -1,7 +1,9 @@
 package org.innovect.assignment.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -9,18 +11,22 @@ import org.innovect.assignment.dto.CustomerDashboardInfoDTO;
 import org.innovect.assignment.dto.SubmitOrderPostDTO;
 import org.innovect.assignment.hateoas.event.ResourceCreatedEvent;
 import org.innovect.assignment.service.PizzaFactory;
+import org.innovect.assignment.utils.LinkUtil;
 import org.innovect.assignment.utils.PizzaShopConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriTemplate;
 
 import com.google.common.base.Preconditions;
 
@@ -42,6 +48,27 @@ public class OrderProcessingController {
 	@Autowired
 	private ApplicationEventPublisher eventPublisher;
 
+
+    @GetMapping("/")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void getOrderUrl(final HttpServletRequest request, final HttpServletResponse response) {
+        final String rootUri = request.getRequestURL()
+            .toString();
+
+        final URI dashboardUri = new UriTemplate("{rootUri}{resource}").expand(rootUri, "dashboard");
+        final String dashboardLink = LinkUtil.createLinkHeader(dashboardUri.toASCIIString(), "collection");
+        response.addHeader("Link1", dashboardLink);
+        
+        final URI verifyUri = new UriTemplate("{rootUri}{resource}").expand(rootUri, "verify");
+        final String verifyLink = LinkUtil.createLinkHeader(verifyUri.toASCIIString(), "collection");
+        response.addHeader("Link2", verifyLink);
+
+        final URI submitUri = new UriTemplate("{rootUri}{resource}").expand(rootUri, "submit");
+        final String submitLink = LinkUtil.createLinkHeader(submitUri.toASCIIString(), "collection");
+        response.addHeader("Link3", submitLink);
+
+    }
+    
 	@ApiOperation(value = "Provides information of Pizza and related items.", response = List.class)
 	@GetMapping("/dashboard")
 	public CustomerDashboardInfoDTO getPizzaAndExtraInfo() {
